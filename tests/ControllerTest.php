d26959e1eb5c3f9db2872e9a551ce70f4a89b9ec
bladedevleoper <jamesjonestred@gmail.com>
2020-12-24 17:59:23 +0000

    update comments

--- tests/ControllerTest.php
+++ tests/ControllerTest.php
@@ -11,11 +11,14 @@ class ControllerTest extends TestCase
     /** @test */
     public function a_view_is_returned_from_controller()
     {
-        //Act
+        //Arrange
         $controller = $this->createMock(IndexController::class);
-
+        
+        //Act
         $controller->method('index')
             ->willReturn('home');
+
+        //Assert
         $this->assertSame('home', $controller->index());
     }    
 }
