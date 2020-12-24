efcf30d7cfd7d81e640040180c33fb55572e2695
bladedevleoper <jamesjonestred@gmail.com>
2020-12-24 17:56:39 +0000

    update to test name

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -5,20 +5,12 @@ namespace Tests;
 use PHPUnit\Framework\TestCase;
 use App\Classes\IndexController;
 
-class ViewTest extends TestCase
+class ControllerTest extends TestCase
 {
-    private IndexController $indexController;
-
-    protected function setUp(): void
-    {
-        $this->indexController = new IndexController();
-        
-    }
 
     /** @test */
-    public function an_instance_of_a_view_is_returned_from_controller()
+    public function a_view_is_returned_from_controller()
     {
-        //TODO mock a view to execute the handle method
         //Act
         $controller = $this->createMock(IndexController::class);
 
