1b642a20ceecc21f4cbd5ba17314c6b2114e43b1
bladedevleoper <jamesjonestred@gmail.com>
2020-12-27 16:36:04 +0000

    small update

--- tests/ParseHtmlTest.php
+++ tests/ParseHtmlTest.php
@@ -4,28 +4,34 @@ namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 use App\Library\Parser\HtmlParser;
-
+/**
+ * @group htmlParser
+ */
 class ParseHtmlTest extends TestCase
 {
+    private $pasrser;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->parser = new HtmlParser();
+    }
+
     /** @test */
     public function can_parse_curley_braces_into_php_tags()
     {
-        //Arrange
-        $parser = new HtmlParser();
         //Act
-        $result = $parser->parse('<h5>{{$hello}}</h5>');
+        $result = $this->parser->parse('<h5>{{$hello}}</h5>');
         $expected = '<h5><?=$hello;?></h5>';
 
         //Assert
         $this->assertEquals($expected, $result);
 
-         //Arrange
-         $parser = new HtmlParser();
-         //Act
-         $result = $parser->parse('<h5>{{ $hello }}</h5>');
-         $expected = '<h5><?=$hello;?></h5>';
+        //Act
+        $result = $this->parser->parse('<h5>{{ $hello }}</h5>');
+        $expected = '<h5><?=$hello;?></h5>';
  
-         //Assert
-         $this->assertEquals($expected, $result);
+        //Assert
+        $this->assertEquals($expected, $result);
     }
 }
