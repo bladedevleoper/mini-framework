63ff40393dc554c6d668d081ecc4222fdfd58154
bladedevleoper <jamesjonestred@gmail.com>
2020-12-29 02:24:27 +0000

    add doc blocks

--- app/Traits/RegexTrait.php
+++ app/Traits/RegexTrait.php
@@ -4,7 +4,13 @@ namespace App\Traits;
 
 trait RegexTrait
 {
-    public function removeSpacesBetweenCurlyBraces($string)
+    /**
+     * will remove white space between curly braces only
+     *
+     * @param string $string
+     * @return void
+     */
+    public function removeSpacesBetweenCurlyBraces(string $string): string
     {
         return preg_replace('/\s+(?=[$])|\s+(?=}})/', '', $string);
     }
